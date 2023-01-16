4d6c2bee789b0b79ddce85dff77a8a2ce2ad9c0d
Lisa Miner <midnightmoet@gmail.com>
2023-01-16 17:22:44 -0500

    not the final version but will push it

--- script.js
+++ script.js
@@ -1,10 +1,128 @@
-const container = document.getElementById('container');
+const DEFAULT_COLOR = '#333333'
+const DEFAULT_MODE = 'color'
+const DEFAULT_SIZE = 16
 
-function makeDivs(numDivs){
-    for(let d = 0; d < numDivs; d++){
-        let cells = document.createElement('div');
-        container.appendChild(cells);
-    }
+
+// Global Variables
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
+
+
+// Allows the user to change the color of the grid
+function setCurrentColor(newColor) {
+  currentColor = newColor
 }
 
-makeDivs(256);
+
+// Allows the user to change the mode of the grid
+function setCurrentMode(newMode) {
+  activateButton(newMode)
+  currentMode = newMode
+}
+
+// Allows the slider to change the grid side and values
+function setCurrentSize(newSize) {
+  currentSize = newSize
+}
+
+// DOM Elements
+const colorPicker = document.getElementById('color-picker')
+const colorBtn = document.getElementById('color-btn')
+const rainbowBtn = document.getElementById('rainbow-btn')
+const eraserBtn = document.getElementById('eraser-btn')
+const clearBtn = document.getElementById('clear-btn')
+const sizeValue = document.getElementById('size-Value')
+const sizeSlider = document.getElementById('size-Slider')
+const grid = document.getElementById('grid')
+
+
+// Event Listeners
+colorPicker.oninput = (e) => setCurrentColor(e.target.value)
+colorBtn.onclick = () => setCurrentMode('color')
+rainbowBtn.onclick = () => setCurrentMode('rainbow')
+eraserBtn.onclick = () => setCurrentMode('eraser')
+clearBtn.onclick = () => reloadGrid()
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value)
+
+
+// Allows the user to draw while holding down the mouse
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+
+// Allows the slider to change the grid size
+function changeSize(value) {
+  setCurrentSize(value)
+  updateSizeValue(value)
+  reloadGrid()
+}
+
+
+// Allows the slider to change the grid side and values
+function updateSizeValue(value) {
+  sizeValue.innerHTML = `${value} x ${value}`
+}
+
+// reloads / clears the grid for new play
+function reloadGrid() {
+  clearGrid()
+  setupGrid(currentSize)
+}
+
+// clears the grid to start fresh
+function clearGrid() {
+  grid.innerHTML = ''
+}
+
+function setupGrid(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement('div')
+    gridElement.classList.add('grid-element')
+    gridElement.addEventListener('mouseover', changeColor)
+    gridElement.addEventListener('mousedown', changeColor)
+    grid.appendChild(gridElement)
+  }
+}
+
+function changeColor(e) {
+  if (e.type === 'mouseover' && !mouseDown) return
+  if (currentMode === 'rainbow') {
+    const randomR = Math.floor(Math.random() * 256)
+    const randomG = Math.floor(Math.random() * 256)
+    const randomB = Math.floor(Math.random() * 256)
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+  } else if (currentMode === 'color') {
+    e.target.style.backgroundColor = currentColor
+  } else if (currentMode === 'eraser') {
+    e.target.style.backgroundColor = '#fefefe'
+  }
+}
+
+function activateButton(newMode) {
+  if (currentMode === 'rainbow') {
+    rainbowBtn.classList.remove('active')
+  } else if (currentMode === 'color') {
+    colorBtn.classList.remove('active')
+  } else if (currentMode === 'eraser') {
+    eraserBtn.classList.remove('active')
+  }
+
+  if (newMode === 'rainbow') {
+    rainbowBtn.classList.add('active')
+  } else if (newMode === 'color') {
+    colorBtn.classList.add('active')
+  } else if (newMode === 'eraser') {
+    eraserBtn.classList.add('active')
+  }
+}
+
+window.onload = () => {
+  setupGrid(DEFAULT_SIZE)
+  activateButton(DEFAULT_MODE)
+}
